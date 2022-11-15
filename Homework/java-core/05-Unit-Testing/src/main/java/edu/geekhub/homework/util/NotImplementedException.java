369a9655395c913c0c8311c6ce1941dff0fc0b18
vrudas <vrudas4work@gmail.com>
2022-11-15 02:05:39 +0200

    GHL-5 Added util exception for tests TODO marks

---
+++ Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/util/NotImplementedException.java
@@ -0,0 +1,22 @@
+package edu.geekhub.homework.util;
+
+public class NotImplementedException extends RuntimeException {
+
+    private NotImplementedException(String message) {
+        super(message);
+    }
+
+    public static void TODO() {
+    }
+
+    public static void TODO(String message) {
+        throw new NotImplementedException("");
+    }
+
+    public static <T> T TODO_TYPE() {
+        return TODO_TYPE("");
+    }
+    public static <T> T TODO_TYPE(String message) {
+        throw new NotImplementedException(message);
+    }
+}
